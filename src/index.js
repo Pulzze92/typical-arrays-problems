5745cde3305605fb06fde024b132c6f9ed53bd03
Pulzze92 <89998310392@mail.ru>
2021-03-10 17:29:20 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+	let m = 0;
+	if(array === undefined || array.length == 0) return 0;
+	for (let i = 0; i < array.length; i++) {
+    if (array[i] < m) {
+			m = array[i];
+}
+} return m;
 }
 
 exports.max = function max (array) {
-  return 0;
+	let m = 0;
+	if(array === undefined || array.length == 0) return 0;
+for (let i = 0; i < array.length; i++) {
+if (array[i] > m) {
+m = array[i];
+}
+} return m;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	let m = 0, j = 0;
+	if(array === undefined || array.length == 0) return j;
+	for (let i = 0; i < array.length; i++) {
+		m += array[i];
+	}
+	m /= array.length;
+	return m;
 }
